4b41aed8fe602c05dba731e3a8a79e3f7d1cfe5c
shinkhan <qlsjtmek2@gmail.com>
2024-09-20 20:39:47 +0900

    Create Scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,12 @@ using System.Reflection;
 
 public class Player : MonoBehaviour
 {
+    public int Level;                // 레벨
+    public int Exp;                  // 경험치
+    public int Gold;                 // 골드
+    public int MaxExp;               // 최대 경험치
+    public int MaxGold;              // 최대 골드
+
     public float MaxHealth;          // 체력
     public float AttackPower;        // 공격력
     public float AttackSpeed;        // 공격속도
